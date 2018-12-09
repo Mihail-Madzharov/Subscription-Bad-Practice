38f42f3fb3e0612042493ecce712d5ef6c9ab0b6
mihail madzharov <mihail.madzharov.m@gmail.com>
2018-12-09 12:47:34 +0200

    Waterfall:logic

--- SubscriptionBadPractice/src/app/waterfall/waterfall.component.ts
+++ SubscriptionBadPractice/src/app/waterfall/waterfall.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-waterfall',
   templateUrl: './waterfall.component.html',
   styleUrls: ['./waterfall.component.css']
 })
-export class WaterfallComponent implements OnInit {
+export class WaterfallComponent {
+  public sourceObsCounter = 0;
+  public secondObsCounter = 0;
 
-  constructor() { }
+  public sourceObs = new BehaviorSubject(0);
+  public secondObs = new BehaviorSubject(0);
 
-  ngOnInit() {
+  constructor() {
+    this.sourceObs
+      .pipe(delay(1000))
+      .subscribe(() => {
+        this.sourceObsCounter += 1;
+        this.secondObs
+          .pipe(delay(1000))
+          .subscribe(() => {
+            this.secondObsCounter += 1;
+          });
+      });
   }
 
+  emitSource() {
+    this.sourceObs.next(0);
+  }
+
+  emitSecondObs() {
+    this.secondObs.next(0);
+  }
 }
